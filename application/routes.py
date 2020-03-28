5cccff0cb7f288aa33d50c2991578a2a7faeb1ab
kirill <tolkun_14@inbox.ru>
2020-03-28 20:22:04 +0300

    Add CORS support (AJAX request fix). Update index mapping in 'routes.py'

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,11 @@
+from flask_cors import cross_origin
+
 from application import app
 
+from flask import json
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+
+@app.route('/', methods=['GET'])
+@cross_origin()
+def hello():
+    return json.dumps({'message': "Backend message."})
