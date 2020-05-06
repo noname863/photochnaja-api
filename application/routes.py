f60f66d8e4979ec9ba79c7601e6f408b341719c6
kirill <tolkun_14@inbox.ru>
2020-05-06 22:23:52 +0300

    Add annotation '@token_required' to check JWT-token before executing route function

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from functools import wraps
 
 import jwt
 from flask import request, jsonify
@@ -7,6 +8,40 @@ from sqlalchemy import or_
 
 from application import app, db
 from application.models import User
+
+
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registeration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            user = User.query.filter_by(login=data['sub']).first()
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(
+                expired_msg), 401  # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
 
 
 @app.route('/signup', methods=['POST'])
