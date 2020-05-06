2cb7dcab0517e7fedc1c6e53c46dd8499644ee5f
kirill <tolkun_14@inbox.ru>
2020-05-06 22:19:13 +0300

    Add JWT package to requirements. Add routing for signin of user with return JWT-token

--- application/routes.py
+++ application/routes.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+import jwt
 from flask import request, jsonify
 from flask_cors import cross_origin
 from sqlalchemy import or_
@@ -19,3 +22,21 @@ def signup():
     db.session.add(user)
     db.session.commit()
     return user.jsonify(), 201
+
+
+@app.route('/signin', methods=['POST'])
+@cross_origin()
+def login():
+    data = request.get_json()
+    user = User.authenticate(**data)
+
+    if not user:
+        return jsonify(
+            {'message': 'Invalid credentials', 'authenticated': False}), 401
+
+    token = jwt.encode({
+        'sub': user.login,
+        'iat': datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=30)},
+        app.config['SECRET_KEY'])
+    return jsonify({'token': token.decode('UTF-8')})
