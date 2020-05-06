91397ddc65b34f43de886f4e911a3428cffa0644
kirill <tolkun_14@inbox.ru>
2020-05-06 21:58:47 +0300

    Add necessary requirements for SQLAlchemy ORM package.Create database object

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config.from_pyfile('settings.py')
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+db = SQLAlchemy(app)
 
 from application import routes
