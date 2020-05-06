0e0bbd655faedb49f77c7e27c7ca22bc752b8f2c
kirill <tolkun_14@inbox.ru>
2020-05-06 20:07:29 +0300

    Configure database (Microsoft SQL Server) connection

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
+app.config.from_pyfile('settings.py')
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 from application import routes
---
+++ application/settings.py
@@ -0,0 +1,33 @@
+import os
+
+############
+# DATABASE #
+############
+
+DATABASE_USER = 'sa'
+if not DATABASE_USER:
+    DATABASE_USER = os.environ['DATABASE_USER']
+
+DATABASE_PASSWORD = 'password1/'
+if not DATABASE_PASSWORD:
+    DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+
+DATABASE_HOST = 'localhost'
+if not DATABASE_HOST:
+    DATABASE_HOST = os.environ['DATABASE_HOST']
+
+DATABASE_PORT = '1433'
+if not DATABASE_PORT:
+    DATABASE_PORT = os.environ['DATABASE_PORT']
+
+DATABASE_NAME = 'photochnaja_db'
+if not DATABASE_NAME:
+    DATABASE_NAME = os.environ['DATABASE_NAME']
+
+DATABASE_DRIVER = 'ODBC+Driver+17+for+SQL+Server'
+if not DATABASE_DRIVER:
+    DATABASE_DRIVER = os.environ['DATABASE_DRIVER']
+
+SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://{}:{}@{}:{}/{}?driver={}'.format(
+    DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT,
+    DATABASE_NAME, DATABASE_DRIVER)
