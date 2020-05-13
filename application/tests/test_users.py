cecba2d89333e2b3e9d882621de26f274d34e47b
Pavel Amelkov <p_amelkov@wargaming.net>
2020-05-13 13:32:44 +0300

    Setup environment

---
+++ application/tests/test_users.py
@@ -0,0 +1,29 @@
+import json
+import unittest
+
+from application import app, db
+
+TEST_DB = 'test.db'
+
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        print(app.config['SQLALCHEMY_DATABASE_URI'])
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        db.drop_all()
