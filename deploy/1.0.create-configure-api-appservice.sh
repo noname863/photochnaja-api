de36bf2dbc2d2215004242feafde370aacf47d72
Bobrov Oleg <olegich051@gmail.com>
2020-05-14 22:44:59 +0300

    Add file '1.0.create-configure-api-appservice.sh' to create and configure api appservice

---
+++ deploy/1.0.create-configure-api-appservice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source 0.0.create-resource-group.sh
+source 1.1.create-api-appservice-plan.sh
+source 1.2.create-api-appservice.sh
+source 1.3.configure-startup-file.sh
+#source 1.4.configure-deployment-source.sh
+source 1.5.configure-https-only.sh
+
+eval $post_command_create_api
