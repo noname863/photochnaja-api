f712de4c0a5af28f8579a5b427417fbf530ec2a5
kirill <tolkun_14@inbox.ru>
2020-05-24 00:47:01 +0300

    Source script `1.6.configure-environment-variables.sh` to set environment variables in app service

--- deploy/1.0.create-configure-api-appservice.sh
+++ deploy/1.0.create-configure-api-appservice.sh
@@ -6,5 +6,6 @@ source 1.2.create-api-appservice.sh
 source 1.3.configure-startup-file.sh
 #source 1.4.configure-deployment-source.sh
 source 1.5.configure-https-only.sh
+source 1.6.configure-environment-variables.sh
 
 eval $post_command_create_api
