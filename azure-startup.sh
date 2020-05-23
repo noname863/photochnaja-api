7a914b572335c58408f297b965fd569837eb1b1c
kirill <tolkun_14@inbox.ru>
2020-05-24 00:07:16 +0300

    Add installing necessary dependencies for 'pyodbc' python package by package manager apt

--- azure-startup.sh
+++ azure-startup.sh
@@ -1,3 +1,10 @@
+# Install dependencies for 'pyodbc' python package
+apt update
+apt install g++ -y
+apt install unixodbc-dev -y
+apt install unixodbc -y
+apt install libodbc1 -y
+
 # Install python packages as application dependencies
 pip install -r requirements.txt
 
