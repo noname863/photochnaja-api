e3a89b505adc9937214fcc3acc84c1a17ce8d695
kirill <tolkun_14@inbox.ru>
2020-05-26 21:36:39 +0300

    Add 'pull-request-workflow.sh' to start github action from pull request workflow

---
+++ .github/workflows/pull-request-workflow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_URL='repo_url'
+AUTH_TOKEN='github_auth_token'
+
+curl --location --request POST "$REPO_URL/dispatches" \
+  --header "Authorization: Bearer $AUTH_TOKEN" \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+    "event_type": "start-pull-request-workflow"
+}'
